5c4e45239167ceb8b59ef7ff940f05c202621bc4
ah-james <11ahj11@gmail.com>
2022-01-24 21:17:50 -0500

    Fetch Requests for Occupation and State Information, Adding that Info to Form Dropdowns

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [occupations, setOccupations] = useState()
+  const [states, setStates] = useState() 
+
+  // GET request to populate select dropdowns in form
+  useEffect(() => {
+    fetch('https://frontend-take-home.fetchrewards.com/form')
+        .then(response => response.json())
+        .then(data => setOccupations(data.occupations));
+
+        fetch('https://frontend-take-home.fetchrewards.com/form')
+        .then(response => response.json())
+        .then(data => setStates(data.states));
+
+  }, []);
+
   return (
-    <form>
-      <label>Full Name: </label>
-      <input name="fullName" type="text" />
-      <br />
-      <label>Email: </label>
-      <input name="email" type="email" />
-      <br />
-      <label>Password: </label>
-      <input name="password" type="password" />
-      <br />
-      <label>Occupation: </label>
-      <select>
-        {/* fill in options through GET request */}
-        <option value="Option 1">Option 1</option>
-      </select>
-      <br/>
-      <label>State:</label>
-      <select>
-        {/* fill in options through GET request */}
-        <option value="Option 1">Option 1</option>
-      </select>
-      <br/>
-      <input type="submit" />
-    </form>
+    <div>
+      <form>
+        <label>Full Name: </label>
+        <input name="fullName" type="text" />
+        <br />
+        <label>Email: </label>
+        <input name="email" type="email" />
+        <br />
+        <label>Password: </label>
+        <input name="password" type="password" />
+        <br />
+        <label>Occupation: </label>
+        <select>
+          {/* fill in options through GET request */}
+          <option value="1">         </option>
+          {occupations?.map((occupation, id) => <option key={occupation} value={id+1}>{occupation}</option>)}
+        </select>
+        <br/>
+        <label>State:</label>
+        <select>
+          {/* fill in options through GET request */}
+          <option value="1">         </option>
+          {states?.map((state, id) => <option key={state.abbreviation} value={id+1}>{state.name}</option>)}
+        </select>
+        <br/>
+        <input type="submit" />
+      </form>
+
+
+    </div>
+
   );
 }
 
